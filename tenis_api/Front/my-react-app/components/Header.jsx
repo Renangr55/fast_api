5e38144f8a1923ef9e7d36e5fe5c573f609281ca
Renan Gabriel Rodrigues Mendon�a <49278351890@>
2025-06-11 17:02:40 -0300

    11/06 - Comecei o front

---
+++ tenis_api/Front/my-react-app/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import airJordan from "../src/assets/air-jordan.png";
+import { TfiAlignJustify } from "react-icons/tfi";
+
+
+export const Header = () => {
+    return (
+        <div className="bg-black w-434 h-100{%} flex justify-between">
+            <img className="w-20 h-20" src={airJordan} alt="" />
+            
+            <nav className="flex justify-center items-center">
+                <ul className="flex justify-center items-center gap-3	">
+                    <li><a href="" className="text-white">Shoes</a></li>
+                    <li><a href="" className="text-white">Brand</a></li>
+                    <li><a href="" className="text-white">JordamX</a></li>
+                </ul>
+            </nav>
+
+            <div className="flex justify-center items-center p-4">
+                < TfiAlignJustify color="white"/>
+            </div>
+
+
+
+            
+
+            
+        </div>
+    )
+}
--- tenis_api/Front/my-react-app/src/App.jsx
+++ tenis_api/Front/my-react-app/src/App.jsx
@@ -3,10 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Home } from '../pages/Home'
+import { Header } from '../components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Home />
   )
